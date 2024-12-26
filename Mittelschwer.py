95d121f090d1f648cde267bd334323486739ab40
Huseyin-can-24 <huseyincan.goktepe@std.yeditepe.edu.tr>
2024-12-26 17:34:06 +0300

    Inp10326.12.2024 Huseyin can

---
+++ Mittelschwer.py
@@ -0,0 +1,15 @@
+n=int(input('Note'))
+s=int(input('Stufe'))
+
+if n>=90 :
+  print('einstellen')
+elif s>=5 and n>=70 :
+  print('einstellen')
+elif n>=70 :
+  print('zum Gespach einladen')
+elif s>=4 :
+  print('zum Gespach einladen')
+else:
+  print('ablehnen')
+print('Ende')
+
