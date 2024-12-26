718ee64ba599246653617ee15d7b601039207c8d
Huseyin-can-24 <huseyincan.goktepe@std.yeditepe.edu.tr>
2024-12-26 16:53:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-a=int(input('Alter'))
-n=int(input('Bessernote'))
-if 50>=a>=20 and n>80:
-  print('Einsteller')
-else :
-  print('Ablehnen')
-print('Ende')
-  
+n=int(input('Note'))
+s=int(input('Stufe'))
 
+if n>=90 :
+  print('einstellen')
+elif s>=5 and n>=70 :
+  print('einstellen')
+elif n>=70 :
+  print('zum Gespach einladen')
+elif s>=4 :
+  print('zum Gespach einladen')
+else:
+  print('ablehnen')
+print('Ende')
 
